992a25052ddaa6f5648247df8490309769b15550
JITHIN K C <jithinkc22@gmail.com>
2020-03-24 22:19:52 +0530

    Update josephus_main.m

--- josephus_main.m
+++ josephus_main.m
@@ -3,3 +3,7 @@
 n=1; 
 k=2; 
 position=josephus(n,k)
+
+% Note: If you are using my code for your system or project, you should always cite my paper as a reference. 
+% K C.Jithin, SyamSankar, "Colour image encryption algorithm combining Arnold map, DNA sequence operation, and a Mandelbrot set", Journal of Information Security and Applications, Elsevier, Volume 50, February 2020, DOI: https://doi.org/10.1016/j.jisa.2019.102428
+
